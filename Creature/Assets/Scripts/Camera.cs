d3fcaf1ff2cb4741ad84ef46b70329bff20ed241
Linus Riddle <linusriddler@gmail.com>
2026-09-22 08:40:52 -0400

    Made Some Basic UI Assets and Unlocked the Cursor During Battle

--- Creature/Assets/Scripts/Camera.cs
+++ Creature/Assets/Scripts/Camera.cs
@@ -1,6 +1,7 @@
 using System.Threading;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class Camera : MonoBehaviour
 {
@@ -12,8 +13,6 @@ public class Camera : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
     }
 
     // Update is called once per frame
@@ -27,5 +26,16 @@ public class Camera : MonoBehaviour
 
         // Rotate vertically around camera's right axis
         transform.RotateAround(target.position, transform.right, -mouseY);
+        // Check whether or not to lock the mouse
+        if (SceneManager.GetActiveScene().name == "Battle")
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
     }
 }
